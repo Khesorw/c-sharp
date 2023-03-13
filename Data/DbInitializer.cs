0d40c12aa64274e032afa6694d67b32e87dccabd
Hasibullah Yosufi <yosu0001@algonquinlive.com>
2023-03-13 01:05:36 -0400

    Connection string configured now starting to code views and controllers

---
+++ Data/DbInitializer.cs
@@ -0,0 +1,12 @@
+﻿namespace Lab4.Data
+{
+    public static class DbInitializer
+    {
+
+        public static void Initialize(NewsDbContext context)
+        {
+            context.Database.EnsureCreated();
+        }
+
+    }
+}
---
+++ Models/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lab4.Models
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
